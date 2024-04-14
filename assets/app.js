756d567bccc18b8ce05b6252ba1ed0147367b987
champion <jphilippe.champion@gmail.com>
2024-04-14 20:03:16 +0200

    progression du projet

--- assets/app.js
+++ assets/app.js
@@ -12,18 +12,41 @@ import './styles/app.css';
 
 $(document).ready(function(){
 
-   // $('.filtre-marque, .filtre-modele, .filtre-motorisation, .filtre-service').hide();
+  $('.filtre-marque, .filtre-modele, .filtre-motorisation, .filtre-service').hide();
 
-    $('#atelier').click(function(){
-       
-      $('.filtre-modele, .filtre-motorisation, .filtre-service').hide();
+    $('#atelier').click(function(event){
+       event.preventDefault();
+      //$('.filtre-marque').hide();
+     $('.filtre-marque').show();
     });
-   $('.marque').click(function(){
-
+   $('.marque').click(function(event){
+       event.preventDefault();
       $('.filtre-modele').show();
-      $('.filtre-motorisation').hide();
-      $('.filtre-service').hide();
+    })
+    $('.modele').click(function(event){
+      event.preventDefault();
+      $('.filtre-motorisation').show();
+    })
+    $('.motorisation').click(function(event){
+      event.preventDefault();
+      $('.filtre-service').show();
     })
 })
 
+$(document).ready(function(){
+  
+  $('.Vmarque, .Vmodele').hide();
+  $('#annonces').click(function(){
+   
+    $('.Vmarque').show();
+   // $('.Vmodele').hide();
+  })
+
+  $('.vmarque').click(function(event){
+    event.preventDefault();
+    $('.Vmodele').show();
+  })
+
+  
+})
 
